59c0e0d12f62edab3a99c11dfa67bea40e4a1a89
Ben Holland <bhollan5@kent.edu>
2021-02-13 20:55:38 -0500

    Restructuring

--- tests/script.js
+++
@@ -1,9 +0,0 @@
-//  testing ktty via script.
-
-//  this section tests the minimum features 
-//  needed to parse & run KttyLab. 
-
-
-
-var KTTY = require( __dirname + "/../ktty.js" );
-var ktty = new KTTY();
