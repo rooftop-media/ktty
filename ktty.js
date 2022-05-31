119894f561eac0324e5a76ee511ee615483f1b50
Ben H <bhollan5@kent.edu>
2022-05-30 21:31:52 -0400

    Part G with working scroll!!

--- ktty.js
+++ ktty.js
@@ -2,63 +2,66 @@
 ;
 
 ////  SECTION 1:  Imports
-//  Importing NodeJS libraries. 
+
+//  Importing NodeJS libraries.                                                                                                                        
 var process      = require("process");
 var fs           = require("fs");
 
+
+
 ////  SECTION 2:  Objects
 
+
 var Buffer = {
-    text:        "",
-    filename:    "",
-    modified:    "",
-    cursor_pos:  0,
+    text:         "",
+    filename:     "",
+    modified:     false,
+    cursor_pos:   0,
     scroll_pos:   0,
-    wrapped_text: "",
-    
+
     focus:             Buffer_focus,
     load_file:         Buffer_load_file,
     get_cursor_coords: Buffer_get_cursor_coords,
     draw:              Buffer_draw,
-    position_cursor:   Buffer_position_cursor,
-    get_wrapped_text:  Buffer_get_wrapped_text,
-    get_visible_chunk: Buffer_get_visible_chunk,
-    scroll:            Buffer_scroll,
 
     events:            {
         "CTRL-C":     function() {  Window.quit()  },
-    
+
         "LEFT":       Buffer_move_cursor_left,
         "RIGHT":      Buffer_move_cursor_right,
         "UP":         Buffer_move_cursor_up,
         "DOWN":       Buffer_move_cursor_down,
-    
+
         "TEXT":       function(key) {  Buffer_add_to_text(key);   },
         "ENTER":      function()    {  Buffer_add_to_text("\n");  },
         "BACKSPACE":  Buffer_delete_from_text,
-    
+
         "CTRL-S":     Buffer_save_to_file,
-    
+
         // "CTRL-Z":     p_undo,                                                                                                                           
         // "CTRL-R":     q_redo, 
     }
-    
+  
 };
 function Buffer_focus() {
     Keyboard.focus_item = this;
+    FeedbackBar.text    = "";
+    FeedbackBar.input   = "";
 }
 function Buffer_load_file() {
     this.filename = process.argv[2];
     if ( this.filename == undefined ) {
-    	FeedbackBar.focus();
-        FeedbackBar.text = "No file name given!  Enter a new filename:";
-        FeedbackBar.confirm_event = function(new_filename) {
-	    Buffer.filename = new_filename;
-	    Buffer.focus();
+	      FeedbackBar.focus();
+	      FeedbackBar.text = "No file name given!  Enter a new filename:";
+	      FeedbackBar.confirm_event = function(new_filename) {
+	      Buffer.filename = new_filename;
+	      Buffer.focus();
 	}
     } else {
         try {
             this.text = fs.readFileSync( this.filename, {encoding: 'utf8'} );
+	          this.text = this.text.replace(/\r\n/g, '\n');
+            this.text = this.text.replace(/\r/g, '');
         } catch (err) {
             FeedbackBar.focus();
             FeedbackBar.text    = "Unable to find a file at '" + this.filepath + "'.  Enter a new filename:";
@@ -70,54 +73,60 @@ function Buffer_load_file() {
         }
     }
 }
-function Buffer_get_cursor_coords() {
-    var cursor_coords = [1,1];
-    for (var i = 0; i < this.cursor_pos; i++) {  //  Loop through the buffer to count \n's 
+function Buffer_get_cursor_coords() {            /**  Returns a 2 index array, [int line, int char]           **/
+
+    var cursor_coords = [1,1];                      //  line, char coord of cursor
+    for (var i = 0; i < this.cursor_pos; i++) {  //  Loop through the buffer to count \n's  
+
         var current = this.text[i];
         if (current == "\n" || cursor_coords[1] >= Window.width - 1) {
             cursor_coords[0]++;        /**  Advance a line.        **/
-            cursor_coords[1] = 1;      /**  Reset character pos.   **/
-        } else {
+	          cursor_coords[1] = 1;      /**  Reset character pos.   **/
+	      } else {
             cursor_coords[1]++;        /**  Advance a character.   **/
         }
     }
-
+    
     cursor_coords[0] -= Buffer.scroll_pos;
     if (cursor_coords[0] <= 0) {
         Buffer.scroll_pos--;
-	    return Buffer.get_cursor_coords();
-    } else if (cursor_coords[0] > Window.height - 5) {
+	      return Buffer.get_cursor_coords();
+    } else if (cursor_coords[0] > Window.height - 3) {
         Buffer.scroll_pos++;
-	    return Buffer.get_cursor_coords();
+	      return Buffer.get_cursor_coords();
     } else {
-    	return cursor_coords;
+    	  return cursor_coords;
     }
+    
 }
 function Buffer_draw() {
 
     console.clear();
     var buff_lines = this.text.split("\n");
-    var overflow   = 1;
+    var overflow   = 0;
 
     for (var i = 0; i < buff_lines.length; i++) {
         var line = buff_lines[i];
+        var overflow_lines = [];
+        while (line.length > Window.width) {                          /**  This WHILE loop breaks down any lines that overflow Window.width.   **/     
+            overflow++;
+            var line_part = line.slice(0, Window.width - 1);
+            overflow_lines.push(line_part);
+            line = line.slice(Window.width - 1, line.length);
+        }
 	
-        if (i >= this.scroll_pos && i < (Window.height + this.scroll_pos - overflow) ) {   /**  This IF statement ensures we draw the correct amount of lines!   **/
-	
-            while (line.length > Window.width) {                          /**  This WHILE loop breaks down any lines that overflow _window_w.   **/     
-                overflow++;
-                var line_part = line.slice(0, Window.width - 1);
-                console.log(line_part + "\x1b[2m\\\x1b[0m");              /**  Dim, add "\", undim   **/
-                line = line.slice(Window.width - 1, line.length);
+        if (
+            i >= this.scroll_pos - overflow &&               /* Start drawing at the line at scroll_pos - overflow */
+            i < (Window.height + this.scroll_pos - overflow - 3) /* Stop drawing at the window height plus this offset. */
+           ) {  
+            for (var j = 0; j < overflow_lines.length; j++) {
+                console.log(overflow_lines[j] + "\x1b[2m\\\x1b[0m");   /**  Dim, add "\", undim   **/
             }
             console.log(line);
         }
     }
 }
-function Buffer_position_cursor() {
-    var cursor_position = this.get_cursor_coords();
-    process.stdout.write("\x1b[" + cursor_position[0] + ";" + cursor_position[1] + "f");
-}
+
 //  Buffer event functions:
 function Buffer_move_cursor_left() {
     Buffer.cursor_pos -= 1;
@@ -138,7 +147,7 @@ function Buffer_move_cursor_right() {
 }
 function Buffer_move_cursor_up() {
     var current_x_pos = 1;               /**   To find the xpos of the cursor on the current line.   **/
-    var prev_line_length = 0;            /**   To find the length of the *prev* line, to jump back.  **/
+    var prev_line_length = -1;           /**   To find the length of the *prev* line, to jump back.  **/
     for (var i = 0; i < Buffer.cursor_pos; i++ ) {
         if (Buffer.text[i] == "\n") {
             prev_line_length = current_x_pos;
@@ -147,7 +156,10 @@ function Buffer_move_cursor_up() {
             current_x_pos++;
         }
     }
-    if (prev_line_length > current_x_pos) {        /**   If we're going up **into** a line...        **/
+    if (prev_line_length == -1) {                  /**   If there is no previous line...              */
+        Buffer.cursor_pos = 0;
+    }
+    else if (prev_line_length > current_x_pos) {   /**   If we're going up **into** a line...        **/
         Buffer.cursor_pos -= prev_line_length;
     }
     else if (prev_line_length <= current_x_pos) {  /**   If we're going up **above** a line...       **/
@@ -189,8 +201,11 @@ function Buffer_move_cursor_down() {
         } //  End of "if" statements
     } //  End of "for" loop
     
-    if (next_line_length >= current_x_pos) {          /**   If we're going down **into** a line...                **/
+    if (!found_line_start) {                          /**   If there is no next line...                    */
         Buffer.cursor_pos += current_line_length;
+    } 
+    else if (next_line_length >= current_x_pos) {     /**   If we're going down **into** a line...        **/
+	Buffer.cursor_pos += current_line_length;
     }
     else if (next_line_length < current_x_pos) {      /**   If we're going down **above** a line...                **/
         Buffer.cursor_pos += current_line_length;
@@ -204,7 +219,6 @@ function Buffer_move_cursor_down() {
     } else {
         FeedbackBar.text = "Moved down.";
     }
-
 }
 function Buffer_add_to_text(new_text) {
     var new_buffer = Buffer.text.slice(0, Buffer.cursor_pos);
@@ -218,7 +232,7 @@ function Buffer_add_to_text(new_text) {
     }
 }
 function Buffer_delete_from_text() {
-    if ( Buffer.cursor_pos == 0 ) {      /**   Don't let the cursor position be negative.    **/
+    if ( Buffer.cursor_position == 0 ) {      /**   Don't let the cursor position be negative.    **/
         return;
     }
     var new_buffer = Buffer.text.slice(0, Buffer.cursor_pos - 1);
@@ -235,30 +249,8 @@ function Buffer_save_to_file() {
     Buffer.modified = false;
     FeedbackBar.text = "saved :)";
 }
-function Buffer_get_wrapped_text() {
-    var lines   = this.text.split("\n");
-    var wrapped = "";
-    for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        while (line.length > Window.width) {
-	    wrapped += line.split(0,Window.width) + "\n";
-	    line     = line.split(Window.width, line.length);
-	}
-	wrapped += line + "\n";
-    }
-    this.wrapped_text = wrapped;
-}
-function Buffer_get_visible_chunk() {
-    var lines   = this.wrapped_text.split("\n");
-    for (var i = this.scroll_pos; i < this.scroll_pos + Window.height; i++) {
-    
-    }
-}
-function Buffer_scroll() {
 
-}
-
-
+//  The status bar
 var StatusBar = { 
     draw:   StatusBar_draw,
 };
@@ -285,6 +277,7 @@ function StatusBar_draw() {
     process.stdout.write("\x1b[0m");                                  /**  No more reverse video.            **/
 }
 
+//  The feedback bar
 var FeedbackBar = {
     text:            "",
     input:           "",
@@ -293,7 +286,7 @@ var FeedbackBar = {
 
     draw:            FeedbackBar_draw,
     focus:           FeedbackBar_focus,
-    position_cursor: FeedbackBar_position_cursor,
+    get_cursor_coords: FeedbackBar_get_cursor_coords,
     
     events:            {
         "CTRL-C":     function() {  Window.quit()  },
@@ -302,7 +295,7 @@ var FeedbackBar = {
         "RIGHT":      FeedbackBar_move_cursor_right,
 
         "TEXT":       function(key) {  FeedbackBar_add_to_text(key);   },
-	    "BACKSPACE":  FeedbackBar_delete_from_text,
+	      "BACKSPACE":  FeedbackBar_delete_from_text,
         "ENTER":      function()    {  FeedbackBar.confirm_event(FeedbackBar.input);  },
     }
 };
@@ -326,9 +319,10 @@ function FeedbackBar_draw() {
     _feedback_bar = "";
     process.stdout.write("\x1b[0m");                               /**  Back to undim text.               **/
 }
-function FeedbackBar_position_cursor() {
+function FeedbackBar_get_cursor_coords() {
     var cursor_x = this.text.length + this.cursor_pos + 2;
-    process.stdout.write("\x1b[" + (Window.height - 1) + ";" + cursor_x + "H");
+    var cursor_y = Window.height - 1;
+    return [cursor_y, cursor_x];
 }
 // FeedbackBar event functions...
 function FeedbackBar_add_to_text(key) {
@@ -336,7 +330,7 @@ function FeedbackBar_add_to_text(key) {
     var new_input = _this.input.slice(0, _this.cursor_pos);
     new_input    += key;
     new_input    += _this.input.slice(_this.cursor_pos, _this.input.length);
-    _this.input    = new_input;
+    _this.input   = new_input;
     _this.cursor_pos++;
 }
 function FeedbackBar_delete_from_text() {
@@ -356,7 +350,7 @@ function FeedbackBar_move_cursor_left() {
         _this.cursor_pos++;
     } else {
         _this.draw();
-        _this.position_cursor();
+	      _this.position_cursor();
     }
 }
 function FeedbackBar_move_cursor_right() {
@@ -366,14 +360,15 @@ function FeedbackBar_move_cursor_right() {
         _this.cursor_pos--;
     } else {
         _this.draw();
-        _this.position_cursor();
+	      _this.position_cursor();
     }
 }
 
+//  The window.
 var Window = {
     height:    100,
     width:     100,
-    
+  
     get_size:  Window_get_size,
     draw:      Window_draw,
     quit:      Window_quit
@@ -383,15 +378,18 @@ function Window_get_size() {
     this.width  = process.stdout.columns;
 }
 function Window_draw() {
+    Keyboard.cursor_coords = Keyboard.focus_item.get_cursor_coords();
+
     Buffer.draw();
     StatusBar.draw();
     FeedbackBar.draw();
-    Keyboard.focus_item.position_cursor();
+    
+    Keyboard.position_cursor();
 }
 function Window_quit() {
     if (!Buffer.modified) {
-         console.clear();
-	 process.exit();
+	      console.clear();
+	      process.exit();
     } else {
         FeedbackBar.focus();
         FeedbackBar.text = "Modified buffer exists!! Save before quitting? (y/n)";
@@ -404,45 +402,48 @@ function Window_quit() {
                 FeedbackBar.focus();
                 FeedbackBar.text = "Quit without saving? Changes will be lost! (y/n)";
                 FeedbackBar.confirm_event = function(response) {
-                if (response.toLowerCase() == "y") {
-                    console.clear();
-                    process.exit();
-                } else {
-                    FeedbackBar.text = "";
-                    Buffer.focus();
+                    if (response.toLowerCase() == "y") {
+                        console.clear();
+                        process.exit();
+                    } else {
+                        FeedbackBar.text = "";
+                        Buffer.focus();
+                    }
                 }
-            }
             } else {
                 FeedbackBar.text = "Modified buffer exists!! Save before quitting? (Respond with 'y' or 'n')";
                 FeedbackBar.input = "";
-                FeedbackBar.cursor_pos = 0;
-            }
-            
+            }	    
         }
     }
 }
 
+//  The keyboard.
 var Keyboard = {
-    focus_item:  Buffer,
+    focus_item:    Buffer,
+    cursor_coords: [0, 0],        /*  Cursor position coordinates: line, char */
 
     event_names: {
-      "\u001b[A": "UP",
-      "\u001b[B": "DOWN",
-      "\u001b[C": "RIGHT",
-      "\u001b[D": "LEFT",
-      "\u007f":   "BACKSPACE",
-      "\u0008":   "BACKSPACE",  /* For powershell */
-      "\u000D":   "ENTER",
-      "\u0003":   "CTRL-C",
-      "\u0013":   "CTRL-S",
+        "\u001b[A": "UP",
+        "\u001b[B": "DOWN",
+        "\u001b[C": "RIGHT",
+        "\u001b[D": "LEFT",
+        "\u007f":   "BACKSPACE",
+        "\u0008":   "BACKSPACE",  /*  for powershell  */
+        "\u000D":   "ENTER",
+        "\u0003":   "CTRL-C",
+        "\u0013":   "CTRL-S",
     },
-    
+  
+    position_cursor: function() {
+        process.stdout.write("\x1b[" + this.cursor_coords[0] + ";" + this.cursor_coords[1] + "f");
+    },
     map_events: Keyboard_map_events
-    
+  
 };
 
 function Keyboard_map_events() {
-    //  Map keyboard input                                                                                                                             
+    //  Map keyboard input                                                                                                                         
     var stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.resume();
@@ -451,20 +452,23 @@ function Keyboard_map_events() {
     var _this = this;
     var key_reaction = function(key) {
 
-        var event_name = _this.event_names[key];         /**  Getting the event name from the keycode, like "CTRL-C" from "\u0003".  **/
+	      var event_name = _this.event_names[key];             /**  Getting the event name from the keycode, like "CTRL-C" from "\u0003".  **/
 
-        if (typeof event_name == "string" && typeof _this.focus_item.events[event_name] == "function") {       /**  "CTRL-C", "ENTER", etc     **/
+        if (typeof event_name == "string" && typeof _this.focus_item.events[event_name] == "function") {   /**  "CTRL-C", "ENTER", etc   **/
             _this.focus_item.events[event_name]();
-        } else if (key.charCodeAt(0) > 31 && key.charCodeAt(0) < 127) {        /**  Most keys, like letters, call the "TEXT" event.  **/
+        } else if (key.charCodeAt(0) > 31 && key.charCodeAt(0) < 127) {            /**  Most keys, like letters, call the "TEXT" event.  **/
             _this.focus_item.events["TEXT"](key);
         }
-        Window.draw();                                   /**  Redraw the whole screen on any keypress.                               **/
+	      Window.draw();                                       /**  Redraw the whole screen on any keypress.                               **/
     };
 
     stdin.on("data", key_reaction);
 }
 
+
 ////  SECTION 3:  Boot stuff. 
+
+//  The boot sequence.
 function boot() {
 
     /**  Load a file to the buffer.       **/
